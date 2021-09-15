51dddd60c1435076ccb6aa45b231687e97d8cf42
Chia-An Yang <chiaan.y.creativeworker@gmail.com>
2021-09-15 22:11:16 +0800

    test

--- models/followship.js
+++ models/followship.js
@@ -1,8 +1,7 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Followship = sequelize.define('Followship', {
-  }, {});
-  Followship.associate = function(models) {
-  };
+  const Followship = sequelize.define("Followship", {}, {});
+  Followship.associate = function (models) {};
+
   return Followship;
 };

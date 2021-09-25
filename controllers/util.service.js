e29e017b48edd057576d79ddedc84bbb3ae4907c
Anna <chiaan.y.creativeworker@gmail.com>
2021-09-25 17:06:50 +0800

    feat: add topuser to followship

--- controllers/util.service.js
+++ controllers/util.service.js
@@ -1,24 +1,32 @@
 const helpers = require("../_helpers.js")
-module.exports = {
-  getTestUser: (req) => {
-    if (process.env.NODE_ENV === "test") {
-      return helpers.getUser(req);
-    } else { return req.user }},
+const db = require("../models")
+const { User } = db;
+const { Op } = require("sequelize");
+const moment = require("moment");
+const listAttributes = ["id", "name", "account", "avatar"];
+
+getTestUser = (req) => {
+  if (process.env.NODE_ENV === "test") {
+    return helpers.getUser(req);
+  } else { return req.user }},
+
+getTopUser = (req, variant) => {
+  User.findAll({
+    attributes: listAttributes,
+    include: [{ model: User, as: "Followers", attributes: ["id"] }],
+    where: {
+      id: { [Op.not]: getTestUser(req).id },
+      role: { [Op.not]: "admin" }}})
+  .then( rawUsers => { 
+    const rankedUsers = rawUsers.map((data) => ({
+      ...data.dataValues,
+      FollowerCount: data.Followers.length,
+      isFollowed: getTestUser(req).Followings.map((d) => d.id).includes(data.id)
+    })).sort((a, b) => b.FollowerCount - a.FollowerCount);
     
-  getTopUser:(req) => {
-    User.findAll({
-      attributes: listAttributes,
-      include: [{ model: User, as: "Followers", attributes: ["id"] }],
-      where: {
-        id: { [Op.not]: user.id },
-        role: { [Op.not]: "admin" }}})
-    .then( rawUsers => { 
-      rawUsers.map((data) => ({
-        ...data.dataValues,
-        FollowerCount: data.Followers.length,
-        isFollowed: this.getTestUser(req).Followings.map((d) => d.id).includes(data.id)
-      }))
-      .sort((a, b) => b.FollowerCount - a.FollowerCount);
-      return Users.slice(0, 10);})
-  } 
-}
+    return variant = rankedUsers.slice(0, 10);})
+} 
+
+
+
+module.exports = { getTestUser, getTopUser }
